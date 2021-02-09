c1b6c84cd451f9d041e896db9afebb8c16331500
Lyang Medeiros <lyang.medeiros@ee.ufcg.edu.br>
2021-02-08 23:31:33 -0300

    Defines the needed addresses

--- Src/main.c
+++ Src/main.c
@@ -19,9 +19,29 @@
 
 #include <stdint.h>
 
-#if !defined(__SOFT_FP__) && defined(__ARM_FP)
-  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
-#endif
+/*
+ * The goal of this exercise is to get 1 KB of STACK memory space
+ * and separate it in two spaces, each one with 512 B. The first
+ * space will be pointed by MSP and the second will be pointed by
+ * PSP.
+ *
+ * As such, when the processor is on Handler Mode, the first portion
+ * will be used as stack memory.
+ *
+ * By consequence, when the processor is on Thread Mode, the second
+ * portion will be used as stack memory.
+ *
+ */
+
+#define S_RAM 		0x20000000U 			// Initial RAM address
+#define RAM_SZ		(128 * 1024) 			// RAM Size = 128 KB
+#define E_RAM 		( (S_RAM) + (RAM_SZ) )	// Final RAM address
+#define S_STACK 	E_RAM					// Stack starts at the end of RAM
+
+#define S_MSP_STACK	S_STACK					// MSP space starts with Stack
+#define E_MSP_STACK ( S_STACK + 512 )		// MSP occupies the first 512 B of Stack
+
+#define S_PSP_STACK E_MSP_STACK				// PSP space starts at the end of MSP space
 
 int main(void)
 {
